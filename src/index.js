2b7d5d5ca3db70740025829636499f0f127ddd06
Vitaly-Nik <nikitalik93@mail.ru>
2023-01-17 03:18:50 +0300

    feat: 16 of 20 tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let strToArr = str.split('');
+let brackets = {};
+let i = 0;
+while( i < bracketsConfig.length){
+  brackets[bracketsConfig[i][0]] = bracketsConfig[i][1]
+  i++;
+}
+let index = -1;
+while (strToArr.length > 1){
+ index = strToArr.indexOf(`${brackets[`${strToArr[0]}`]}`, 1)
+ if (index > 0){
+  strToArr.splice(index, 1);
+  strToArr.shift();
+ } 
+ else {
+  return false;
+ }
+}
+if (strToArr.length === 1){
+  console.log("false2");
+  return false;
+} else if (strToArr.length === 0){
+  console.log("true1");
+  return true;
+}
 }

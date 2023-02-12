11407ce1b1af3c8f13f4b52b109650cb941bf7e9
Vitaly-Nik <nikitalik93@mail.ru>
2023-02-12 14:44:04 +0300

    refactor: refactored 'brackets', 'arr.forEach' and 'return'

--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ module.exports = function check(str, bracketsConfig) {
 let arr = str.split('');
 let stack = [];
 let brackets = {};
-for (let i = 0; i < bracketsConfig.length; i++){
-  brackets[`${bracketsConfig[i][0]}`] = `${bracketsConfig[i][1]}`
-}
 
-arr.forEach(e => {
-  if (e != brackets[stack[stack.length - 1]]){
-    stack.push(e)
-  } else {
-    stack.pop()
-  }
+bracketsConfig.forEach(e => {
+  brackets[e[0]] = e[1]
 })
 
-if (stack.length === 0){
-  return true;
-} else {
-  return false
-}
+arr.forEach(e => {
+  e === brackets[stack[stack.length - 1]] ? stack.pop() : stack.push(e)
+})
+
+return stack.length === 0 ? true : false 
 }
